09c7f873da04353fe8e3e275b3aae894b7c9c51e
eglereact <eglereactjs@gmail.com>
2022-03-14 17:36:59 +0200

    recoil and moment

--- components/Post.js
+++ components/Post.js
@@ -1,4 +1,5 @@
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import {
   BsChat,
   BsThreeDots,
@@ -7,11 +8,20 @@ import {
   BsFillHeartFill,
 } from 'react-icons/bs';
 import { FiRepeat, FiShare } from 'react-icons/fi';
+import Moment from 'react-moment';
+import { useRecoilState } from 'recoil';
+import { modalState, postIdState } from '../atoms/modalAtom';
 
 function Post({ id, post, postPage }) {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
+  const router = useRouter();
   return (
-    <div className="flex cursor-pointer border-b border-[#F3F4F6] p-3">
+    <div
+      className="flex cursor-pointer border-b border-[#F3F4F6] p-3"
+      onClick={() => router.push(`/${id}`)}
+    >
       {!postPage && (
         <img
           src={post?.userImg}
@@ -45,7 +55,7 @@ function Post({ id, post, postPage }) {
             </div>{' '}
             ·{' '}
             <span className="text-sm hover:underline sm:text-[15px]">
-              {/* <Moment fromNow>{post?.timestamp?.toDate()}</Moment> */}
+              <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
             </span>
             {!postPage && (
               <p className="mt-0.5 text-[15px] text-[#0F1419] sm:text-base">
@@ -72,18 +82,32 @@ function Post({ id, post, postPage }) {
             postPage && 'mx-auto'
           }`}
         >
-          <div className="icon group">
-            <BsChat className="h-5 group-hover:text-[#1d9bf0]" />
+          {/* Block for the comments */}
+          <div
+            className="group flex items-center space-x-1"
+            onClick={(e) => {
+              e.stopPropagation();
+              setPostId(id);
+              setIsOpen(true);
+            }}
+          >
+            <div className="icon group">
+              <BsChat className="h-5 group-hover:text-[#1d9bf0]" />
+            </div>
           </div>
+          {/* Block for the retweet only visual */}
           <div className="icon group hover:bg-[#37C897] hover:bg-opacity-10">
             <FiRepeat className="h-5 group-hover:text-[#37C897]" />
           </div>
+          {/* Block for the likes */}
           <div className="icon group hover:bg-[#F9318D] hover:bg-opacity-10">
             <BsHeart className="h-5 group-hover:text-[#F9318D]" />
           </div>
+          {/* Block for the share only visual */}
           <div className="icon group">
             <FiShare className="h-5 group-hover:text-[#1d9bf0]" />
           </div>
+          {/* Block for the delete post */}
           <div className="icon group hover:bg-[#fc4655] hover:bg-opacity-10">
             <BsTrash className="h-5 group-hover:text-[#fc4655]" />
           </div>
